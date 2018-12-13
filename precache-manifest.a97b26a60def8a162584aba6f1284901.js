196babfe1e481559bf6cd82c45b44e0ddb2af13d
Gabriel <1707gabri@gmail.com>
2018-12-13 16:00:00 +0100

    Updates

---
+++ precache-manifest.a97b26a60def8a162584aba6f1284901.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ebcbdc6efc924d5ff2",
+    "url": "/whiteboard/static/js/runtime~main.31ebcbdc.js"
+  },
+  {
+    "revision": "b348615a2defc19ae62a",
+    "url": "/whiteboard/static/js/main.b348615a.chunk.js"
+  },
+  {
+    "revision": "4e86de8fb0f46688758f",
+    "url": "/whiteboard/static/js/1.4e86de8f.chunk.js"
+  },
+  {
+    "revision": "b348615a2defc19ae62a",
+    "url": "/whiteboard/static/css/main.5b7dac72.chunk.css"
+  },
+  {
+    "revision": "90c56e821342846877bb4123a23246e7",
+    "url": "/whiteboard/index.html"
+  }
+];
