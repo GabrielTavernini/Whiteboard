bdc20e5dfb8280ff7f5ae2661310843303d7d66d
Gabriel <1707gabri@gmail.com>
2018-12-14 20:37:44 +0100

    New favicon

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const  socket = io('http://192.168.0.106:8000')//'https://worldwhiteboard.herokuapp.com');
+const  socket = io('http://localhost:8000')//'https://worldwhiteboard.herokuapp.com');
 
 function subscribeToNewNotes(cb, reset) {
   socket.on('newNote', (note) => cb(null, note));
