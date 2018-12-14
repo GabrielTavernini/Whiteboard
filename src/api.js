419720e4fa3fbfcf7b1c9b984a42af46b3dadcfc
Gabriel <1707gabri@gmail.com>
2018-12-14 20:16:51 +0100

    New phone scale

--- src/api.js
+++ src/api.js
@@ -2,11 +2,8 @@ import io from 'socket.io-client';
 const  socket = io('http://192.168.0.106:8000')//'https://worldwhiteboard.herokuapp.com');
 
 function subscribeToNewNotes(cb, reset) {
-  //socket.on('timer', timestamp => cb(null, timestamp));
-  //socket.emit('subscribeToTimer', 10000);
   socket.on('newNote', (note) => cb(null, note));
   socket.on('resetNotes', (notesArr) => reset(notesArr));
-  //socket.emit('newNote', 'Fucking long note jks jfkafkafhjj sasads ssss  sss sss ss s hjhj', 'Author', '100', '20', '0.7');
 }
 
 function sendNote(note, author, x, y){
